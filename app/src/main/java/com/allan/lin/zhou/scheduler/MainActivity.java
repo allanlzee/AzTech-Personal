bec00c38149952e0bba375e52c6b9d1fcb065adb
Allan Zhou <allanlinzhou@gmail.com>
2021-06-26 19:25:42 -0400

    Added Main Navigation Menu

--- app/src/main/java/com/allan/lin/zhou/scheduler/MainActivity.java
+++ app/src/main/java/com/allan/lin/zhou/scheduler/MainActivity.java
@@ -1,26 +1,59 @@
 package com.allan.lin.zhou.scheduler;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    // View header = navigationView.getHeaderView(0);
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button click = findViewById(R.id.button);
-        click.setOnClickListener(new View.OnClickListener(){
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar =  findViewById(R.id.toolbar);
 
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Wassup", Toast.LENGTH_SHORT).show();
-            }
-        });
+        setSupportActionBar(toolbar); // Theme.AppCompat.NoActionBar
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_open, R.string.menu_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Make the Menu Items Clickable
+        // navigationView.setNavigationItemSelectedListener(this); // TODO: this line crashes the app
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
