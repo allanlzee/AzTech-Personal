a9019b87b189e5adcd1774a6caff2c2534c226fd
Allan Zhou <allanlinzhou@gmail.com>
2021-06-27 21:55:52 -0400

    Added Snackbar for Mindfulness

--- app/src/main/java/com/allan/lin/zhou/scheduler/Mindfulness.java
+++ app/src/main/java/com/allan/lin/zhou/scheduler/Mindfulness.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.allan.lin.zhou.scheduler.databinding.MindfulnessActivityBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Mindfulness extends AppCompatActivity {
 
@@ -28,11 +29,18 @@ public class Mindfulness extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Mindfulness.this, MainActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                Snackbar.make(view, "Back to Home", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Go", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(Mindfulness.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }).setActionTextColor(getResources().getColor(R.color.home_action))
+                        .setTextColor(getResources().getColor(R.color.home_snack))
+                        .show();
             }
         });
-
     }
 }
