42ce3fb47cdfa64d876ccbdba9509d844b625586
Allan Zhou <allanlinzhou@gmail.com>
2021-06-27 21:54:28 -0400

    Added Floating Button, Removed Menu

--- app/src/main/java/com/allan/lin/zhou/scheduler/Mindfulness.java
+++ app/src/main/java/com/allan/lin/zhou/scheduler/Mindfulness.java
@@ -1,42 +1,38 @@
 package com.allan.lin.zhou.scheduler;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
-import com.google.android.material.navigation.NavigationView;
+import com.allan.lin.zhou.scheduler.databinding.MindfulnessActivityBinding;
 
 public class Mindfulness extends AppCompatActivity {
 
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
     Toolbar toolbar;
+    private MindfulnessActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mindfulness_activity);
 
-        /* drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_view); */
-
         toolbar = findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
 
-        /* navigationView.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_open, R.string.menu_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+        binding = MindfulnessActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        navigationView.setNavigationItemSelectedListener(this); */
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Mindfulness.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
