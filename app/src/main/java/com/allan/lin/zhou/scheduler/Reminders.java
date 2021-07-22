ed6763770da1b6322232a0a2ddaf2292d759d25f
Allan Zhou <allanlinzhou@gmail.com>
2021-07-21 23:40:21 -0400

    Added Remove Singular Reminder

--- app/src/main/java/com/allan/lin/zhou/scheduler/Reminders.java
+++ app/src/main/java/com/allan/lin/zhou/scheduler/Reminders.java
@@ -10,10 +10,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.allan.lin.zhou.scheduler.databinding.RemindersActivityBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -89,7 +91,28 @@ public class Reminders extends AppCompatActivity {
         ReminderAdapter reminderAdapter = new ReminderAdapter(getApplicationContext(), daily);
         reminderList.setAdapter(reminderAdapter);
         reminderList.setClickable(true);
+
+        reminderList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Snackbar.make(view, "Delete Reminder?", Snackbar.LENGTH_SHORT)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                daily.remove(position);
+                                Reminder.allReminders.remove(position);
+                                Toast.makeText(Reminders.this, "Reminder Removed", Toast.LENGTH_SHORT).show();
+                                reminderAdapter.notifyDataSetChanged();
+                            }
+                        }).setActionTextColor(Reminders.this.getResources().getColor(R.color.home_action))
+                        .setTextColor(Reminders.this.getResources().getColor(R.color.home_snack))
+                        .show();
+            }
+        });
     }
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
