a360355bc938049ab2eded9f56687e863ef32aaa
Allan Zhou <allanlinzhou@gmail.com>
2021-08-03 23:20:22 -0400

    Reviewed All Bugs and Problems

--- app/src/main/java/com/allan/lin/zhou/scheduler/PersonalAccount.java
+++ app/src/main/java/com/allan/lin/zhou/scheduler/PersonalAccount.java
@@ -1,17 +1,38 @@
 package com.allan.lin.zhou.scheduler;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Patterns;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.allan.lin.zhou.scheduler.databinding.PersonalAccountActivityBinding;
+import com.allan.lin.zhou.scheduler.ui.login.Preferences;
+import com.allan.lin.zhou.scheduler.ui.login.firebase.Constants;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 public class PersonalAccount extends AppCompatActivity {
 
     private PersonalAccountActivityBinding binding;
     private Toolbar toolbar;
+    private Preferences preferenceManager;
+    private String encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +46,31 @@ public class PersonalAccount extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        preferenceManager = new Preferences(getApplicationContext());
+        initializeData();
+
+        binding.saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (isValidSignUpDetails()) {
+                    saveData();
+                } else {
+                    showToast("Invalid Editing!");
+                }
+            }
+        });
+
+        binding.profilePicture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                pickImage.launch(intent);
+            }
+        });
     }
 
     @Override
@@ -34,5 +80,97 @@ public class PersonalAccount extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    private void initializeData() {
+        binding.editTextName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        binding.editTextEmail.setText(preferenceManager.getString(Constants.KEY_EMAIL));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.profilePicture.setImageBitmap(bitmap);
+        String greeting = "Welcome Back " + preferenceManager.getString(Constants.KEY_NAME) + "!";
+        binding.editTextGreeting.setText(greeting);
+        binding.editTextPassword.setText(preferenceManager.getString(Constants.KEY_PASSWORD));
+    }
+
+    private void saveData() {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put(Constants.KEY_NAME, binding.editTextName.getText().toString());
+        user.put(Constants.KEY_EMAIL, binding.editTextEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD, binding.editTextPassword.getText().toString());
+        user.put(Constants.KEY_IMAGE, encodedImage);
+
+        // Send to Firestore Database
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                    preferenceManager.putString(Constants.KEY_USER_ID, documentReference.getId());
+                    preferenceManager.putString(Constants.KEY_NAME, binding.editTextName.getText().toString());
+                    preferenceManager.putString(Constants.KEY_IMAGE, encodedImage);
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    Utilities.isLoggedIn = true;
+                })
+                .addOnFailureListener(exception -> {
+                    showToast(exception.getMessage());
+                });
+    }
+
+    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    if (result.getData() != null) {
+                        Uri imageUri = result.getData().getData();
+                        try {
+                            InputStream inpStream = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inpStream);
+                            binding.profilePicture.setImageBitmap(bitmap);
+                            encodedImage = encodeImage(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+
+    private String encodeImage(Bitmap bitmap) {
+        int width = 150;
+        int height = bitmap.getHeight() * width / bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    private Boolean isValidSignUpDetails() {
+        if (binding.editTextName.getText().toString().trim().isEmpty()) {
+            showToast("Enter Name");
+            return false;
+        } else if (binding.editTextEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter Email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.editTextEmail.getText().toString()).matches()) {
+            showToast("Enter Valid Email");
+            return false;
+        } else if (binding.editTextPassword.getText().toString().trim().isEmpty()) {
+            showToast("Enter Password");
+            return false;
+        } else if (binding.editTextPassword.getText().toString().trim().isEmpty()) {
+            showToast("Confirm Password");
+            return false;
+        }
+
+        return true;
     }
 }
