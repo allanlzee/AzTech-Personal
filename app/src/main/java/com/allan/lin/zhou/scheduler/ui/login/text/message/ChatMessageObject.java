a95744a903cbc6a21e0846b9942e30266d8c0eac
Allan Zhou <allanlinzhou@gmail.com>
2021-08-05 23:02:44 -0400

    Added Functionality to Send Messages to the Recycler View

--- app/src/main/java/com/allan/lin/zhou/scheduler/ui/login/text/message/ChatMessageObject.java
+++ app/src/main/java/com/allan/lin/zhou/scheduler/ui/login/text/message/ChatMessageObject.java
@@ -1,5 +1,9 @@
 package com.allan.lin.zhou.scheduler.ui.login.text.message;
 
+import java.util.Date;
+
 public class ChatMessageObject {
     public String senderID, receiverID, messageContent, messageDateTime;
+
+    public Date dateTimeObject;
 }
--- app/src/main/java/com/allan/lin/zhou/scheduler/ui/login/text/message/TextMessaging.java
+++ app/src/main/java/com/allan/lin/zhou/scheduler/ui/login/text/message/TextMessaging.java
@@ -14,11 +14,17 @@ import com.allan.lin.zhou.scheduler.ui.login.Preferences;
 import com.allan.lin.zhou.scheduler.ui.login.adapters.TextMessagesAdapter;
 import com.allan.lin.zhou.scheduler.ui.login.firebase.Constants;
 import com.allan.lin.zhou.scheduler.ui.login.firebase.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class TextMessaging extends AppCompatActivity {
 
@@ -57,6 +63,8 @@ public class TextMessaging extends AppCompatActivity {
                 sendMessage();
             }
         });
+
+        // listenMessages();
     }
 
     // Loads User Data into the Text Message Layout
@@ -85,5 +93,61 @@ public class TextMessaging extends AppCompatActivity {
         message.put(Constants.KEY_DATETIME, new Date());
         database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
         binding.textMessageInput.setText(null);
+    }
+
+    private String getDateTime(Date date) {
+        return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
+    }
+
+    private final EventListener<QuerySnapshot> eventListener = (value, error) -> {
+        if (error != null) {
+            return;
+        }
+
+        if (value != null) {
+            int size = textMessages.size();
+            for (DocumentChange documentChange : value.getDocumentChanges()) {
+
+                if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                    // Set variables of the ChatMessageObject to send messages to the recycler view
+                    ChatMessageObject chatMessageObject = new ChatMessageObject();
+                    chatMessageObject.senderID = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                    chatMessageObject.receiverID = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                    chatMessageObject.messageContent = documentChange.getDocument().getString(Constants.KEY_MESSAGE);
+                    chatMessageObject.messageDateTime = getDateTime(documentChange.getDocument().getDate(Constants.KEY_DATETIME));
+                    chatMessageObject.dateTimeObject = documentChange.getDocument().getDate(Constants.KEY_DATETIME);
+
+                    // Add to ArrayList for Recycler View
+                    textMessages.add(chatMessageObject);
+                }
+            }
+
+            // Sort Messages by Time of Sending
+            Collections.sort(textMessages, (dateTime1, dateTime2) ->
+                    dateTime1.dateTimeObject.compareTo(dateTime2.dateTimeObject));
+
+            if (size == 0) {
+                textMessagesAdapter.notifyDataSetChanged();
+            } else {
+                textMessagesAdapter.notifyItemRangeInserted(size, size);
+                binding.textMessagesRecyclerView.smoothScrollToPosition(textMessages.size() - 1);
+            }
+            binding.textMessagesRecyclerView.setVisibility(View.VISIBLE);
+        }
+        binding.progressBar.setVisibility(View.GONE);
+    };
+
+    private void listenMessages() {
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, userRecipient.id)
+                .addSnapshotListener(eventListener);
+
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID, userRecipient.id)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+
+
     }
 }
